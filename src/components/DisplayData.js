e148c9947ae4d11126658ebf69f927da4d7824e4
unknown <anujraghuwanshi2407@gmail.com>
2023-08-27 13:46:44 +0530

    done

---
+++ src/components/DisplayData.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+ const DisplayData = () => {
+
+  const[data , setData] = useState([])
+
+  useEffect(()=>{
+      function fetch(){
+          axios.get(" https://dummyjson.com/products").then((response)=>setData(response.data.products))
+      }
+      fetch()
+  } , [])
+  return (
+    <div>
+        <h1>Data Fetched from API</h1>
+        <pre>
+        {JSON.stringify(data, null, 2)}
+        </pre>
+    </div>
+  )
+}
+
+export default DisplayData
